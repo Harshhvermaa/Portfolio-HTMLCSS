b1315aec3bc2787dcb62fe7df15fcea8ad893743
Aditya <jainaditya879@gmail.com>
2023-11-26 17:47:12 +0530

    banner changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,14 @@
 
 //     }
 //  }
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector('#navbar');
+
+    window.onscroll = function () {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            header.style.backgroundColor = '#fff'; // Change the background color on scroll
+        } else {
+            header.style.backgroundColor = 'transparent'; // Set it back to transparent if not scrolled
+        }
+    };
+});
